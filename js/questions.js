207a0e3f3cdb2517323c849465f5c80b0e0092fd
Joshua <106904667+Joshua635@users.noreply.github.com>
2024-09-03 08:34:04 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,238 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How can we select an element with a specific Class in CSS?",
+    answer: ".",
+    options: [
+      "#",
+      ".",
+      "^",
+      "None of the above"
+    ]
+  },
+  
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which CSS property is used to create an image reflection?",
+    answer: "box-reflect",
+    options: [
+      "box-reflect",
+      "image-reflect",
+      "reflect-img",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question: "How can we change the text color of an element?",
+    answer: "color",
+    options: [
+      "background color",
+      "color",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In How many ways can CSS be written in?",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What type of CSS is generally recommended for designing large web pages?",
+    answer: "External",
+    options: [
+      "Inline",
+      "Internal",
+      "External",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Can negative values be allowed in padding property?",
+    answer: "No",
+    options: [
+      "Yes",
+      "No",
+      "Depends on Property",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "How can we select an element with a specific ID in CSS?",
+    answer: "#",
+    options: [
+      "#",
+      ".",
+      "^",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 11,
+    question: "The CSS property used to specify the transparency of an element is?",
+    answer: "opacity",
+    options: [
+      "opacity",
+      "visibility",
+      "filter",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which of the following are valid CSS position property values?",
+    answer: "All of the above",
+    options: [
+      "static",
+      "relative",
+      "fixed",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Setting an inline-block in CSS requires which of the following properties?",
+    answer: "display",
+    options: [
+      "display",
+      "color",
+      "block",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which of the folowing CSS property specifies the type of list item marker?",
+    answer: "list-style-type",
+    options: [
+      "list",
+      "list-style-type",
+      "ul",
+      "ol"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which of the following properties is used to align text in CSS?",
+    answer: "text-align",
+    options: [
+      "text-align",
+      "text-alignment",
+      "text",
+      "text-position"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which of the following CSS properties specifies the stack order of elements ?",
+    answer: "z-index",
+    options: [
+      "z-index",
+      "overlap",
+      "No Such specific property is present",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Which of the following are units of realtive length in CSS?",
+    answer: "All of the above",
+    options: [
+      "em",
+      "rem",
+      "vmax",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 18,
+    question: "What are the different types of gradients in CSS?",
+    answer: "All of the above",
+    options: [
+      "Linear Gradients",
+      "Conic Gradients",
+      "Radial Gradients",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 19,
+    question: "How many layout models were there before the Flexbox Layout Module was introduced in CSS?",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Whta function is used to insert values of a CSS variable?",
+    answer: "var()",
+    options: [
+      "var()",
+      "rand()",
+      "varchar()",
+      "calc()"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
